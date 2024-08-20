f6b2fa869dadeaf033946285b99db5c03880898b
Matt Clemens <mattclemens@arizona.edu>
2024-08-20 16:45:44 -0700

    final final thing

--- Code/AggClusterSemanticLLJ.py
+++ Code/AggClusterSemanticLLJ.py
@@ -2,18 +2,18 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import AgglomerativeClustering
 
-# Load your DataFrame
+
 dfllj_algo = pd.read_csv('/Users/matt/Desktop/LLJ-Data/LLJSpectrum/YearAlteredData/dflljclusterNomask.csv')
 dfllj_algo['Title'] = dfllj_algo['Title'].astype(str)
 
-# Load the SentenceTransformer model
+
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Generate embeddings for the titles
+
 embeddingsllj = model.encode(dfllj_algo['Title'].tolist(), show_progress_bar=True)
 dfllj_algo['Embeddings'] = list(embeddingsllj)
 
-# Function to save clusters to a file
+
 def save_clusters_to_file(df, cluster_column, filename):
     unique_clusters = sorted(df[cluster_column].unique())
     with open(filename, 'w') as file:
@@ -24,10 +24,10 @@ def save_clusters_to_file(df, cluster_column, filename):
                 file.write(f"    {title}\n")
             file.write("\n")
 
-# List of cluster sizes to try
+
 n_clusters_list = [10, 20, 30, 40, 50, 60, 70]
 
-# Loop through the different cluster sizes
+
 for n_clusters in n_clusters_list:
     agg_clustering_llj = AgglomerativeClustering(
         n_clusters=n_clusters,
