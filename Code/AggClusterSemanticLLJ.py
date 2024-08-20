c1a5e53d11738c43ee9e3993efd47038ac6b3891
Matt Clemens <mattclemens@arizona.edu>
2024-08-20 16:43:18 -0700

    final attempt

--- Code/AggClusterSemanticLLJ.py
+++ Code/AggClusterSemanticLLJ.py
@@ -3,17 +3,17 @@ from sentence_transformers import SentenceTransformer
 from sklearn.cluster import AgglomerativeClustering
 
 # Load your DataFrame
-dfspec_algo = pd.read_csv('/Users/matt/Desktop/LLJ-Data/LLJSpectrum/YearAlteredData/dspeccluster1.csv')
-dfspec_algo['Title'] = dfspec_algo['Title'].astype(str)
+dfllj_algo = pd.read_csv('/Users/matt/Desktop/LLJ-Data/LLJSpectrum/YearAlteredData/dflljclusterNomask.csv')
+dfllj_algo['Title'] = dfllj_algo['Title'].astype(str)
 
-# Initialize the SentenceTransformer model
+# Load the SentenceTransformer model
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Generate embeddings using Sentence Transformers
-embeddingsspec = model.encode(dfspec_algo['Title'].tolist(), show_progress_bar=True)
-dfspec_algo['Embeddings'] = list(embeddingsspec)
+# Generate embeddings for the titles
+embeddingsllj = model.encode(dfllj_algo['Title'].tolist(), show_progress_bar=True)
+dfllj_algo['Embeddings'] = list(embeddingsllj)
 
-# Function to save clusters to a text file
+# Function to save clusters to a file
 def save_clusters_to_file(df, cluster_column, filename):
     unique_clusters = sorted(df[cluster_column].unique())
     with open(filename, 'w') as file:
@@ -24,8 +24,8 @@ def save_clusters_to_file(df, cluster_column, filename):
                 file.write(f"    {title}\n")
             file.write("\n")
 
-# List of different cluster sizes to try
-n_clusters_list = [10, 20, 30, 40, 50, 60, 71]
+# List of cluster sizes to try
+n_clusters_list = [10, 20, 30, 40, 50, 60, 70]
 
 # Loop through the different cluster sizes
 for n_clusters in n_clusters_list:
@@ -35,14 +35,15 @@ for n_clusters in n_clusters_list:
         linkage='average',
     )
     
-    # Perform clustering
-    dfspec_algo['Clusters'] = agg_clustering_llj.fit_predict(embeddingsspec)
     
-    # Save clusters to a text file
-    file_name = f"AggClusteringSpec_{n_clusters}.txt"
-    save_clusters_to_file(dfspec_algo, 'Clusters', file_name)
+    dfllj_algo['Clusters'] = agg_clustering_llj.fit_predict(embeddingsllj)
+    
+   
+    file_name = f"AggClusteringLLJExplode_{n_clusters}.txt"
+    save_clusters_to_file(dfllj_algo, 'Clusters', file_name)
     print(f"Clusters have been saved to {file_name}")
     
-    # Add Cluster column with 'Cluster X' format
-    dfspec_algo['Cluster'] = 'Cluster ' + dfspec_algo['Clusters'].astype(str)
+ 
+    dfllj_algo['Cluster'] = 'Cluster ' + dfllj_algo['Clusters'].astype(str)
+
 
